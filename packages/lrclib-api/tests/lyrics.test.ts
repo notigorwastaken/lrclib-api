953301117402352bf9c58824b95218d09d34c0c8
Igor <80823011+igorwastaken@users.noreply.github.com>
2025-09-14 01:29:30 +0000

    Update

--- packages/lrclib-api/tests/lyrics.test.ts
+++ packages/lrclib-api/tests/lyrics.test.ts
@@ -1,4 +1,4 @@
-import { Client, solveChallenge } from "../src";
+import { Client } from "../src";
 
 describe("song lyrics", () => {
   const client = new Client();
@@ -101,11 +101,11 @@ describe("search song lyrics", () => {
             expect(result.plainLyrics?.split("\n").length).toBeGreaterThan(10); // Ensure multiple lines in plain lyrics
             expect(result.syncedLyrics?.split("\n").length).toBeGreaterThan(10); // Ensure multiple lines in synced lyrics
             */
-  });
+  });/*
   test("If challenge key is returning something", async () => {
     const challenge = await client.requestChallenge();
     console.log(
       solveChallenge(challenge?.prefix as string, challenge?.target as string),
     );
-  });
+  });*/
 });
